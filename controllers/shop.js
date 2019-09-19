0123cf5643b5c8adbe43d787b10b88bc98c705f2
pankaj610 <vermapankaj62@gmail.com>
2019-09-19 21:04:12 +0530

    Cart Added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,33 +1,84 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
-exports.getAllProducts = (req, res, next)=>{ 
-    Product.fetchAll(products=>{
-        res.render('shop/products', { pageTitle: 'Shop', 
-        prods: products,
-        path:'/shop/products' }); 
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
     });
+  });
 };
 
-exports.getIndex = (req, res, next) =>{
-    Product.fetchAll(products=>{
-        res.render('shop/', { 
-            pageTitle: 'Shop', 
-            prods: products,
-            path:'/shop' 
-        });
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
     });
+  });
 };
 
-exports.getCart = (req, res, next) =>{ 
-    res.render('shop/cart', { 
-        pageTitle: 'Cart',
-        path:'/shop/cart' 
-    }); 
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
 };
 
-exports.getCheckout = (req, res, next) =>{ 
-    res.render('shop/checkout', { 
-        pageTitle: 'Checkout',
-        path:'/shop/checkout' 
-    }); 
+exports.getCart = (req, res, next) => {
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
 };
